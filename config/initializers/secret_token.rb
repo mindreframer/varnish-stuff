e5348faa8a41cf062bc1ff665ff6d8c813807e37
Roman Heinrich <roman.heinrich@gmail.com>
2013-09-21 13:23:44 +0200

    Squashed 'github.com/jcihocki/varnishd-skillshare/' content from commit aa67d23
    git-subtree-dir: github.com/jcihocki/varnishd-skillshare
    git-subtree-split: aa67d23730eaeb57bf4fcf71c8cab055e15fdb46

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VarnishdSkillshare::Application.config.secret_token = '6a2d933dba2f28623443c5d0c9e3e6b9595f0e8393557f652c195bd7df883a140595f8d26ef8dda624549b809663cd17b5f2386860928b247e88c68c273993c1'
