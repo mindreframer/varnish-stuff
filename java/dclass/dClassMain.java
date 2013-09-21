f02da8af0d1cfff9a624adcd741008be852fe69e
Roman Heinrich <roman.heinrich@gmail.com>
2013-09-21 16:56:04 +0200

    Squashed 'github.com/TheWeatherChannel/dClass/' content from commit a98b47a
    git-subtree-dir: github.com/TheWeatherChannel/dClass
    git-subtree-split: a98b47ad63f3862e2852bfea93617f2209eef4dd

---
+++ java/dclass/dClassMain.java
@@ -0,0 +1,174 @@
+package dclass;
+
+import java.util.Map;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class dClassMain
+{
+    private static final boolean DTREE_LOG_UALOOKUP=true;
+
+    public static void main(String[] args) throws Throwable
+    {
+        int ret,i;
+        int argc=args.length;
+        int openddr=0;
+        long count,total;
+        long startn,diffn;
+        String loadFile="../dtrees/openddr.dtree";
+        String outFile=null;
+        String parameter=null;
+        dClass di;
+        Map<String,String> kv;
+
+        System.out.println("dClass (version "+dClass.getVersion()+" "+dClass.getAddressing()+"bit addressing "+dClass.getNodeSize()+"byte dt_node)");
+
+        for(i=0;i<argc;i++)
+        {
+            //-l [dtree file]
+            if(args[i].equals("-l") && argc>(++i))
+                loadFile=args[i];
+            //-o [output dtree file]
+            else if(args[i].equals("-o") && argc>(++i))
+                outFile=args[i];
+            //-m [open ddr resource dir]
+            else if(args[i].equals("-d") && argc>(++i))
+            {
+                openddr=1;
+                loadFile=args[i];
+            }
+            else if(args[i].startsWith("-h") || args[i].startsWith("--h"))
+            {
+                System.out.println("Usage: dclass_client [OPTIONS] [FILE|STRING]\n");
+                System.out.println("  -l <path>            load dtree from file");
+                System.out.println("  -o <path>            store dtree to file");
+                System.out.println("  -d <folder path>     load OpenDDR resources xmls");
+                System.out.println("  FILE                 STRING text file");
+                System.out.println("  STRING               test string");
+                
+                return;
+            }
+            //[test string] | [test file]
+            else
+                parameter=args[i];
+        }
+
+        System.out.println("Loading "+(openddr>0?"openddr":"dtree")+": '"+loadFile+"'");
+
+        startn=System.nanoTime();
+
+        di=new dClass(loadFile);
+
+        ret=di.init();
+
+        diffn=System.nanoTime()-startn;
+
+        System.out.println("load dtree tokens: "+ret+" time: "+getTime(diffn));
+
+        System.out.println("dtree stats: nodes: "+di.getNodes()+" mem: "+di.getMemory());
+
+        System.out.println("dtree comment: '"+di.getComment()+"'");
+
+        startn=System.nanoTime();
+
+        ret=(int)di.walk();
+
+        diffn=System.nanoTime()-startn;
+
+        System.out.println("walk tree: "+ret+" tokens "+di.getNodes()+" nodes time: "+getTime(diffn));
+
+        if(outFile!=null)
+        {
+            System.out.println("Dumping dtree: '"+outFile+"'");
+        
+            ret=di.write(outFile);
+        
+            System.out.println("Wrote "+ret+" entries");
+        }
+
+        String test="Mozilla/5.0 (Linux; U; Android 2.2; en; HTC Aria A6380 Build/ERE27) AppleWebKit/540.13+ (KHTML, like Gecko) Version/3.1 Mobile Safari/524.15.0";
+
+        startn=System.nanoTime();
+
+        kv=di.classify(test);
+
+        diffn=System.nanoTime()-startn;
+
+        System.out.println("HTC Aria lookup: '"+kv.get("id")+"' time: "+getTime(diffn));
+
+        if(parameter==null);
+        else if((new File(parameter)).exists())
+        {
+            System.out.println("UA file: '"+parameter+"'");
+
+            count=0;
+            total=0;
+
+            try
+            {
+                BufferedReader in=new BufferedReader(new FileReader(parameter));
+                String line;
+
+                while((line=in.readLine())!=null)
+                {
+                    if(DTREE_LOG_UALOOKUP)
+                        System.out.println("UA: '"+line+"'");
+
+                    startn=System.nanoTime();
+
+                    kv=di.classify(line);
+
+                    diffn=System.nanoTime()-startn;
+
+                    total+=diffn;
+                    count++;
+
+                    if(DTREE_LOG_UALOOKUP)
+                        System.out.println("UA lookup "+count+": '"+kv.get("id")+"' time: "+getTime(diffn));
+                }
+
+                in.close();
+            }
+            catch(Exception ex)
+            {
+            }
+
+            if(count==0)
+                count=1;
+
+            total/=count; 
+
+            System.out.println("TOTAL average time: "+count+", "+getTime(diffn));
+        }
+        else
+        {
+            System.out.println("UA: '"+parameter+"'");
+
+            startn=System.nanoTime();
+
+            kv=di.classify(parameter);
+
+            diffn=System.nanoTime()-startn;
+
+            System.out.println("Param UA lookup: '"+kv.get("id")+"' time: "+getTime(diffn));
+
+            System.out.print("OpenDDR attributes => ");
+
+            for(String key:kv.keySet())
+            {
+                String value=kv.get(key);
+                System.out.print(key+": '"+value+"' ");
+            }
+
+            System.out.println("");
+        }
+
+        di.free();
+    }
+
+    public static String getTime(long diffn)
+    {
+        return diffn/(1000*1000*1000)+"s "+diffn/(1000*1000)%(1000*1000)+"ms "+diffn/1000%1000+"us "+diffn%1000+"ns";
+    }
+}
---
+++ src/dclass_client.c
@@ -0,0 +1,316 @@
+/*
+ * Copyright 2012 The Weather Channel
+ * Copyright 2013 Reza Naghibi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+
+#include "dclass_client.h"
+
+
+//cnode
+typedef struct
+{
+    int                  pos;
+
+    const dtree_dt_node  *cn;
+}
+dclass_cnode;
+
+
+char **dclass_get_value_pos(dclass_keyvalue*,char*);
+static const dclass_keyvalue *dclass_get_kverror(const dclass_index*);
+static char *dclass_error_string(const dtree_dt_index*);
+
+extern unsigned int dtree_hash_char(char);
+extern char *dtree_node_path(const dtree_dt_index*,const dtree_dt_node*,char*);
+
+
+//classifies a string
+const dclass_keyvalue *dclass_classify(const dclass_index *di,const char *str)
+{
+    int on=0;
+    int valid;
+    int bcvalid;
+    int i;
+    int pos=0;
+    int rpos=0;
+    unsigned int hash;
+    char buf[DTREE_DATA_BUFLEN];
+    const char *p;
+    const char *token="";
+    packed_ptr pp;
+    const dtree_dt_node *cnode=NULL;
+    const dtree_dt_node *wnode=NULL;
+    const dtree_dt_node *nnode=NULL;
+    const dtree_dt_node *fbnode;
+    const dtree_dt_node *fnode;
+    const dtree_dt_index *h=&di->dti;
+    dclass_cnode cnodes[DTREE_S_MAX_CHAIN];
+    
+    if(!str || !h->head)
+        return dclass_get_kverror(di);
+    
+    memset(cnodes,0,sizeof(cnodes));
+    
+    dtree_printd(DTREE_PRINT_CLASSIFY,"dclass_classify() UA: '%s'\n",str);
+    
+    for(p=str;*p;p++)
+    {
+        valid=0;
+
+        hash=dtree_hash_char(*p);
+        
+        if(hash<DTREE_HASH_SEP && (hash || *p=='0'))
+        {
+            //new token found
+            if(!on)
+            {
+                token=p;
+                on=1;
+
+                rpos++;
+
+                if(pos<DTREE_S_MAX_POS)
+                    pos++;
+            }
+            
+            valid=1;
+        }
+        
+        if((!valid || (!*(p+1))) && on)
+        {
+            //EOT found
+            fbnode=dtree_get_node(h,token,0,0);
+            
+            dtree_printd(DTREE_PRINT_CLASSIFY,"dtree_classify() token %d(%d): '%s' = '%s':%d\n",
+                    pos,rpos,token,fbnode?dtree_node_path(h,fbnode,buf):"",fbnode?(int)fbnode->flags:0);
+            
+            if(fbnode && dtree_get_flag(h,fbnode,DTREE_DT_FLAG_TOKEN,pos))
+            {
+                if((fnode=dtree_get_flag(h,fbnode,DTREE_DT_FLAG_STRONG,pos)))
+                    return fnode->payload;
+                else if((fnode=dtree_get_flag(h,fbnode,DTREE_DT_FLAG_WEAK,pos)))
+                {
+                    i=DTREE_DC_DISTANCE(h,(char*)fnode->payload);
+                    if(!wnode || fnode->rank>wnode->rank || (fnode->rank==wnode->rank &&
+                            i>=0 && i<DTREE_DC_DISTANCE(h,(char*)wnode->payload)))
+                        wnode=fnode;
+                }
+                else if((fnode=dtree_get_flag(h,fbnode,DTREE_DT_FLAG_NONE,pos)))
+                {
+                    i=DTREE_DC_DISTANCE(h,(char*)fnode->payload);
+                    if(!nnode || (i>=0 && i<DTREE_DC_DISTANCE(h,(char*)nnode->payload)))
+                        nnode=fnode;
+                }
+                
+                if((fnode=dtree_get_flag(h,fbnode,DTREE_DT_FLAG_ACHAIN,pos)))
+                {
+                    pp=fnode->curr;
+
+                    while(pp)
+                    {
+                        bcvalid=0;
+                        
+                        if(fnode->flags & DTREE_DT_FLAG_ACHAIN)
+                        {   
+                            if(fnode->cparam)
+                                dtree_printd(DTREE_PRINT_CLASSIFY,"dtree_classify() looking for pchain %p at dir: %d\n",fnode->cparam,fnode->dir);
+                            else
+                                dtree_printd(DTREE_PRINT_CLASSIFY,"dtree_classify() pchain candidate %p\n",fnode->payload);
+
+                            for(i=0;i<DTREE_S_MAX_CHAIN && cnodes[i].cn;i++)
+                            {
+                                //chain hit
+                                if(cnodes[i].cn->payload==fnode->cparam && fnode->dir<=0)
+                                {
+                                    dtree_printd(DTREE_PRINT_CLASSIFY,"dtree_classify() pchain match: %p('%d'):%d %d\n",cnodes[i].cn->payload,i,cnodes[i].pos,rpos);
+
+                                    if(fnode->dir<0 && cnodes[i].pos-rpos<fnode->dir)
+                                        continue;
+                                    else if(fnode->flags & DTREE_DT_FLAG_BCHAIN)
+                                        bcvalid=1;
+                                    else if(!fnode->rank)
+                                        return fnode->payload;
+                                    else if(!cnode || fnode->rank>cnode->rank)
+                                        cnode=fnode;
+                                }
+
+                                if(cnodes[i].cn->cparam==fnode->payload && cnodes[i].cn->dir>0)
+                                {
+                                    dtree_printd(DTREE_PRINT_CLASSIFY,"dtree_classify() pchain forward match: %p('%d'):%d %d\n",fnode->payload,i,cnodes[i].pos,rpos);
+
+                                    if(rpos-cnodes[i].pos>cnodes[i].cn->dir)
+                                        continue;
+                                    //forward chain has a dependency, not implemented
+                                    else if(fnode->cparam);
+                                    else if(!cnodes[i].cn->rank)
+                                        return cnodes[i].cn->payload;
+                                    else if(!cnode || cnodes[i].cn->rank>cnode->rank)
+                                        cnode=cnodes[i].cn;
+                                }
+                            }
+                        }
+                        
+                        if(fnode->flags & DTREE_DT_FLAG_ACHAIN && (bcvalid || !fnode->cparam || fnode->dir>0))
+                        {
+                            for(i=0;i<DTREE_S_MAX_CHAIN;i++)
+                            {
+                                if(!cnodes[i].cn)
+                                {
+                                    cnodes[i].cn=fnode;
+                                    cnodes[i].pos=rpos;
+
+                                    dtree_printd(DTREE_PRINT_CLASSIFY,"dtree_classify() pchain added: %p('%d'):%d\n",fnode->payload,i,rpos);
+
+                                    break;
+                                }
+                            }
+                        }
+                        
+                        pp=fnode->dup;
+                        fnode=DTREE_DT_GETPP(h,pp);
+                    }
+                }
+            }
+
+            if(!*(p+1) && pos<DTREE_S_MAX_POS)
+            {
+                p--;
+
+                if(token==str)
+                    pos=DTREE_S_BE_POS;
+                else
+                    pos=DTREE_S_MAX_POS;
+    
+                continue;
+            }
+
+            on=0;
+        }
+    }
+    
+    if(cnode)
+    {
+        if(wnode && wnode->rank>cnode->rank)
+            return wnode->payload;
+
+        return cnode->payload;
+    }
+    else if(wnode)
+        return wnode->payload;
+    else if(nnode)
+        return nnode->payload;
+    
+    return dclass_get_kverror(di);
+}
+
+
+//gets a string
+const dclass_keyvalue *dclass_get(const dclass_index *di,const char *str)
+{
+    const dtree_dt_node *node;
+    const dtree_dt_index *h=&di->dti;
+
+    if(!str || !h->head)
+        return dclass_get_kverror(di);
+
+    node=dtree_get_node(h,str,DTREE_DT_FLAG_TOKEN,0);
+    
+    if(node && node->payload)
+        return node->payload;
+
+    return dclass_get_kverror(di);
+}
+
+
+//gets the value for a key
+const char *dclass_get_kvalue(const dclass_keyvalue *kvd,const char *key)
+{
+    int i;
+    
+    for(i=0;i<kvd->size;i++)
+    {
+        if(!strcasecmp(kvd->keys[i],key))
+            return kvd->values[i];
+    }
+    
+    return NULL;
+}
+
+//gets the value pointer for a key
+char **dclass_get_value_pos(dclass_keyvalue *kvd,char *key)
+{
+    int i;
+    
+    for(i=0;i<kvd->size;i++)
+    {
+        if(!strcasecmp(kvd->keys[i],key))
+            return (char**)&kvd->values[i];
+    }
+    
+    return NULL;
+}
+
+//returns an error kvdata
+static const dclass_keyvalue *dclass_get_kverror(const dclass_index *di)
+{
+    if(!di->error.id)
+        ((dclass_index*)di)->error.id=dclass_error_string(&di->dti);
+    
+    return &di->error;
+}
+
+//show first string on error
+static char *dclass_error_string(const dtree_dt_index *h)
+{
+    if(DTREE_M_LOOKUP_CACHE && h->dc_cache[0])
+        return h->dc_cache[0];
+    
+    return DTREE_M_SERROR;
+}
+
+//gets the id for a generic payload
+const char *dclass_get_id(void *payload)
+{
+    dclass_keyvalue *kvd;
+    
+    kvd=(dclass_keyvalue*)payload;
+    
+    return kvd->id;
+}
+
+//version
+const char *dclass_get_version()
+{
+    return DCLASS_VERSION;
+}
+
+//init
+void dclass_init_index(dclass_index *di)
+{
+    memset(&di->error,0,sizeof(dclass_keyvalue));
+    
+    dtree_init_index(&di->dti);
+}
+
+//free
+void dclass_free(dclass_index *di)
+{
+    dtree_free(&di->dti);
+    
+    memset(&di->error,0,sizeof(dclass_keyvalue));
+}
+
