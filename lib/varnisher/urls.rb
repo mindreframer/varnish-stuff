3f7e02768d730e9ed0f4d3fffd04f964529921b5
Roman Heinrich <roman.heinrich@gmail.com>
2013-10-21 16:51:06 +0200

    Squashed 'github.com/robmiller/varnisher/' changes from ca1fa14..50fdc36
    50fdc36 Urls#make_uris shouldn't write @urls
    e3be223 Use assert_includes and refute_includes in tests
    c909d50 Coerce URL strings to URI objects
    10125d9 More efficient normalising of URLs by query string
    5af4d38 Refactor spider
    a4ad7eb Refactor page purger
    
    git-subtree-dir: github.com/robmiller/varnisher
    git-subtree-split: 50fdc36be48cc99ea215e550692bf0e733ab1415

---
+++ lib/varnisher/urls.rb
@@ -0,0 +1,92 @@
+module Varnisher
+  # A collection for URLs, that exposes some useful behaviour (like
+  # selecting only URLs that have a given hostname, or converting
+  # relative URLs to absolute).
+  class Urls
+    include Enumerable
+    extend Forwardable
+
+    # Given an array of URLs (either strings or URI objects), store them
+    # in the collection.
+    def initialize(urls = [])
+      @urls = Array(urls)
+      @urls = make_uris
+    end
+
+    # Coerces the values of the current collection into being URI
+    # objects, which allows strings to be passed initially.
+    def make_uris
+      coerced = urls.map do |url|
+        begin
+          URI(url)
+        rescue
+          nil
+        end
+      end
+
+      coerced.compact
+    end
+
+    # Given a relative URL and a base to work from, will return the
+    # absolute form of that URL.
+    #
+    # For example:
+    #
+    #   absolute_url('http://www.example.com', '/foo')
+    #   # => "http://www.example.com/foo"
+    #
+    #   absolute_url('http://www.example.com/foo', 'bar')
+    #   # => "http://www.example.com/bar"
+    #
+    #   absolute_url('http://www.example.com/foo/bar', 'baz')
+    #   # => "http://www.example.com/foo/baz"
+    def absolute_url(base, url)
+      URI.join(base, URI.escape(url.to_s))
+    end
+
+    # Returns a new collection containing absolute versions of all the
+    # URLs in the current collection.
+    def make_absolute(base)
+      Urls.new(urls.map { |uri| absolute_url(base, uri) })
+    end
+
+    # Returns a new collection containing only the URLs in this
+    # collection that match the given hostname.
+    def with_hostname(hostname)
+      Urls.new(urls.select { |uri| uri.scheme == 'http' && uri.host == hostname })
+    end
+
+    # Returns a new collection containing the URLs in the current
+    # collection, normalised according to their hash.
+    def without_hashes
+      normalised = urls.group_by do |url|
+        url.fragment = nil
+        url
+      end
+
+      Urls.new(normalised.keys)
+    end
+
+    # Returns a new collection containing the URLs in the current
+    # collection without their query string values.
+    def without_query_strings
+      normalised = urls.group_by do |h|
+        url.query = nil
+        url
+      end
+
+      Urls.new(normalised.keys)
+    end
+
+    # Allows the addition of two collections by accessing the underlying
+    # array.
+    def +(other)
+      Urls.new(urls + other.urls)
+    end
+
+    def_delegators :urls, :each, :<<, :length, :empty?, :include?
+
+    protected
+    attr_reader :urls
+  end
+end
